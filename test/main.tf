0e067582e45ef1abafb49b35b196faa52ecd66c7
Rob Levi <robert.levi@digital.justice.gov.uk>
2022-10-04 20:07:24 +0100

    feat: adds mandatory permissions boundary variable
    BREAKING CHANGE: a permission boundary must be provided to prevent privilege escalation

--- test/main.tf
+++ test/main.tf
@@ -1,7 +1,6 @@
 module "example" {
   source = "./.."
 
-
   description         = "github-repo-test"
   template_owner      = "moj-analytical-services"
   template_repository = "rshiny-template-405"
@@ -14,6 +13,6 @@ module "example" {
 
   organisation                = "moj-analytical-services"
   openid_connect_provider_arn = "arn:aws:iam::525294151996:oidc-provider/token.actions.githubusercontent.com"
-
+  permissions_boundary        = "arn:aws:iam::01234567901:policy/boundary-policy"
 
 }
